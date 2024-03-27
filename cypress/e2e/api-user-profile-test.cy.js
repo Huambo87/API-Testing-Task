f536427cb483c6a1aafb940b2fc8f0455aede7c9
Huambo87 <maximgladkov@hotmail.com>
2024-03-27 19:58:07 -0400

    Changes to be committed: 	modified:   cypress/e2e/api-user-profile-test.cy.js

--- cypress/e2e/api-user-profile-test.cy.js
+++ cypress/e2e/api-user-profile-test.cy.js
@@ -1,5 +1,6 @@
 ///<reference types = 'Cypress'/>
 
+
 describe('User Profile Test', () => {
 
     it('male user profile sample', () => {
@@ -9,11 +10,11 @@ describe('User Profile Test', () => {
 
             console.log(response),
             expect(result).has.property('age').to.satisfy(Number.isInteger),
-            expect(result).has.property('city'),
-            expect(result).has.property('gender').eq('male')
-            expect(result).has.property('id').eq(15),
-            expect(result).has.property('name'),
-            expect(result).has.property('registrationDate').contain('201')
+            expect(result).has.property('city').to.be.a('string'),
+            expect(result).has.property('gender').eq('male').to.be.a('string'),
+            expect(result).has.property('id').eq(15).to.satisfy(Number.isInteger),
+            expect(result).has.property('name').to.be.a('string'),
+            expect(result).has.property('registrationDate').to.be.a('string')
             //Since the date is presented as a string, the simplest way to set the cutoff year 
             // without parsing is to specify the first three digits of the year 
         })
@@ -26,24 +27,24 @@ describe('User Profile Test', () => {
 
             console.log(response),
             expect(result).has.property('age').to.satisfy(Number.isInteger),
-            expect(result).has.property('city'),
-            expect(result).has.property('gender').eq('female')
-            expect(result).has.property('id').eq(5),
-            expect(result).has.property('name'),
-            expect(result).has.property('registrationDate').contain('201')
+            expect(result).has.property('city').to.be.a('string'),
+            expect(result).has.property('gender').eq('female').to.be.a('string'),
+            expect(result).has.property('id').eq(5).to.satisfy(Number.isInteger),
+            expect(result).has.property('name').to.be.a('string'),
+            expect(result).has.property('registrationDate').to.be.a('string')
             //Since the date is presented as a string, the simplest way to set the cutoff year 
             // without parsing is to specify the first three digits of the year 
         })
     })
 
     // Negative test: User ID 15 is a duplicate and known to belong to a male user
-    it('female user profile sample', () => {
+    it('female user id duplicate', () => {
         cy.request('/user/15').then((response) => {
 
             var result = response.body.user
 
             console.log(response),
-            expect(result).has.property('age').to.satisfy(Number.isInteger),
+            expect(result).has.property('age'),
             expect(result).has.property('city'),
             expect(result).has.property('gender').eq('female')
             expect(result).has.property('id').eq(15),
@@ -51,6 +52,25 @@ describe('User Profile Test', () => {
             expect(result).has.property('registrationDate').contain('201')
             //Since the date is presented as a string, the simplest way to set the cutoff year 
             // without parsing is to specify the first three digits of the year 
+        })
+    })
+
+    // // Negative test: User ID is invalid
+    it('invalid user id test', () => {
+        cy.request('/user/12345678').then((response) => {
+            console.log(response)
+            // expect(response.body.isSuccess).eq(false),
+            // expect(response.body).has.property('errorCode').eq(404)
+        })
+    })
+
+    // // Negative test: Missing user ID
+    it('missing user id test', () => {
+        cy.request('/user/').then((response) => {
+            console.log(response),
+            expect(response.body.isSuccess).eq(false),
+            expect(response.body).has.property('errorCode').eq(404),
+            expect(response.body).has.property('error').eq('Not Found')
         })
     })
 })
